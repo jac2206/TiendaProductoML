cf471fb99b004601629c725d1934db5b7920d2e0
jac2206 <arango773@gmail.com>
2022-07-10 14:28:09 -0500

    Se creo la arquitectura y organizacion de carpetas del proyecto

---
+++ src/app/modules/administrador-productos/ui/containers/home-productos-container/home-productos-container.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeProductosContainerComponent } from './home-productos-container.component';
+
+describe('HomeProductosContainerComponent', () => {
+  let component: HomeProductosContainerComponent;
+  let fixture: ComponentFixture<HomeProductosContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeProductosContainerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HomeProductosContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
