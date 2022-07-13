d82300378791c7d1b007b81e24b3caccf42d39c2
jac2206 <arango773@gmail.com>
2022-07-13 12:06:10 -0500

    Borro comentarios consumo api

--- src/app/modules/administrador-productos/infraestructura/productos-service.service.ts
+++ src/app/modules/administrador-productos/infraestructura/productos-service.service.ts
@@ -26,41 +26,6 @@ export class ProductosServiceService {
           .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
     }
 
-
-  //Local
-  // public BuscarProductos(search: string) {
-  //   return this.http.get<ListaProductosDTO>(`http://localhost:3000/api/items?q=${search}`)
-  //       .pipe(map(listaProductos => listaProductos), catchError(this.HandleError));
-  // }
-
-  // public ObtenerDetalleProducto(id: string) {
-  //   return this.http.get<DetalleProductoDTO>(`http://localhost:3000/api/items/${id}`)
-  //       .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
-  // }
-
-  // //Azure
-  // public BuscarProductos(search: string) {
-  //   return this.http.get<ListaProductosDTO>(`http://tiendaproductosmlapi.azurewebsites.net/api/items?q=${search}`)
-  //       .pipe(map(listaProductos => listaProductos), catchError(this.HandleError));
-  // }
-
-  // public ObtenerDetalleProducto(id: string) {
-  //   return this.http.get<DetalleProductoDTO>(`http://tiendaproductosmlapi.azurewebsites.net/api/items/${id}`)
-  //       .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
-  // }
-
-
-  //con proxu\y
-  // public BuscarProductos(search: string) {
-  //   return this.http.get<ListaProductosDTO>(`/api/items?q=${search}`)
-  //       .pipe(map(listaProductos => listaProductos), catchError(this.HandleError));
-  // }
-
-  // public ObtenerDetalleProducto(id: string) {
-  //   return this.http.get<DetalleProductoDTO>(`/api/items/${id}`)
-  //       .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
-  // }
-
   private HandleError(res: HttpErrorResponse | any) {
     console.error(res);
     return observableThrowError(res || 'Server error');
