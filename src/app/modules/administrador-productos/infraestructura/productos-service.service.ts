540ed9f5a0166b904043882590d803465727fd5b
jac2206 <arango773@gmail.com>
2022-07-13 12:04:11 -0500

    se agrego configuracion ambiente pruebas y produccion para consumo de la api

--- src/app/modules/administrador-productos/infraestructura/productos-service.service.ts
+++ src/app/modules/administrador-productos/infraestructura/productos-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { catchError, map, throwError as observableThrowError } from 'rxjs';
 import { DetalleProductoDTO } from '../dominio/entidades/detalle-producto-dto';
 import { ListaProductosDTO } from '../dominio/entidades/lista-productos-dto';
+import {environment} from '../../../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,20 @@ export class ProductosServiceService {
     private http: HttpClient
   ) { }
 
+    //Azure
+    public BuscarProductos(search: string) {
+      alert(environment.apiProductos)
+      return this.http.get<ListaProductosDTO>(`${environment.apiProductos}/api/items?q=${search}`)
+          .pipe(map(listaProductos => listaProductos), catchError(this.HandleError));
+    }
+
+    public ObtenerDetalleProducto(id: string) {
+      return this.http.get<DetalleProductoDTO>(`${environment.apiProductos}/api/items/${id}`)
+          .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
+    }
+
+
+  //Local
   // public BuscarProductos(search: string) {
   //   return this.http.get<ListaProductosDTO>(`http://localhost:3000/api/items?q=${search}`)
   //       .pipe(map(listaProductos => listaProductos), catchError(this.HandleError));
@@ -23,16 +38,16 @@ export class ProductosServiceService {
   //       .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
   // }
 
-  //Azure
-  public BuscarProductos(search: string) {
-    return this.http.get<ListaProductosDTO>(`http://tiendaproductosmlapi.azurewebsites.net/api/items?q=${search}`)
-        .pipe(map(listaProductos => listaProductos), catchError(this.HandleError));
-  }
+  // //Azure
+  // public BuscarProductos(search: string) {
+  //   return this.http.get<ListaProductosDTO>(`http://tiendaproductosmlapi.azurewebsites.net/api/items?q=${search}`)
+  //       .pipe(map(listaProductos => listaProductos), catchError(this.HandleError));
+  // }
 
-  public ObtenerDetalleProducto(id: string) {
-    return this.http.get<DetalleProductoDTO>(`http://tiendaproductosmlapi.azurewebsites.net/api/items/${id}`)
-        .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
-  }
+  // public ObtenerDetalleProducto(id: string) {
+  //   return this.http.get<DetalleProductoDTO>(`http://tiendaproductosmlapi.azurewebsites.net/api/items/${id}`)
+  //       .pipe(map(detalleProducto => detalleProducto), catchError(this.HandleError));
+  // }
 
 
   //con proxu\y
