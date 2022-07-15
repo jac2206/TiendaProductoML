11db419eb9e97a4eff0730f0dbfac7bc14d8eb54
jac2206 <arango773@gmail.com>
2022-07-14 21:18:23 -0500

    Se agrego enum para codigos http y clase util para los mensajes de error

--- src/app/modules/administrador-productos/ui/components/detalle-productos/detalle-productos.component.ts
+++ src/app/modules/administrador-productos/ui/components/detalle-productos/detalle-productos.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { filter, map, of } from 'rxjs';
 import { ItemsDTO } from 'src/app/shared/common/entidades/items-dto';
+import { MensajeRespuestaEstadohttp } from 'src/app/shared/common/util/mensajerespuestaestadohttp';
 import { ProductosServiceService } from '../../../infraestructura/productos-service.service';
 
 @Component({
@@ -18,7 +19,8 @@ export class DetalleProductosComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private apiDetalleProducto: ProductosServiceService
+    private apiDetalleProducto: ProductosServiceService,
+    private mensajesHttp: MensajeRespuestaEstadohttp
   ) { }
 
   ngOnInit(): void {
@@ -39,18 +41,8 @@ export class DetalleProductosComponent implements OnInit {
         this.producto = detalleProducto.item;
       },
       error: (err) => {
-        let errHandled = false;
-        if (err instanceof HttpErrorResponse) {
-          const httpErr = err as HttpErrorResponse;
-          if (httpErr.status === 404) {
-            errHandled = true;
-            this.router.navigate(['/']);
-            alert(`El producto ${id} no existe`);
-          }
-        }
-        if (!errHandled) {
-          alert('U Estamos con dificultades técnicas. Por favor intenta más tarde');
-        }
+        alert(this.mensajesHttp.ErrorConsularListaProductos(err));
+        this.router.navigate(['/']);
       }
     });
   }
